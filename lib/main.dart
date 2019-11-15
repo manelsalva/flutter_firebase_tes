44a291aa5baa28f8235ad6b408f7c2aed1f53741
manelsalva <manel.salvador@estudiant.upc.edu>
2019-11-15 16:22:52 +0100

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hola'),
+        ),
+      ),
     );
   }
 }
